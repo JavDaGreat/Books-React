3c9a26faad5184f6bcca0c988a2daf4f6ac21b5c
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-03-08 19:19:22 +0100

    LIst of books persisted with outside API DONE!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import BookCreate from "./Components/BookCreate";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import BookList from "./Components/BookList";
+import axios from "axios";
 
 function App(){
   const [Books,setBooks]=useState([])
 
-  
-  const createBook= (title)=>{
-    const updatedBooks=[...Books,{id: Math.round(Math.random() * 9999),title:title}]
+  const fetchBooks= async()=>{
+    const resp=await axios.get('http://localhost:3001/books')
+    setBooks(resp.data)
+  }
+
+  useEffect(()=>{
+    fetchBooks()
+  },[])
+  const createBook=async (title)=>{
+    const resp= await axios.post('http://localhost:3001/books',{
+      title:title
+    })
+    const updatedBooks=[...Books,resp.data]
     setBooks(updatedBooks)
 
   }
-  const deleteBookId=(id)=>{
+  const deleteBookId=async(id)=>{
+    await axios.delete('http://localhost:3001/books/'+id)
     const updatedBooks=Books.filter((book)=>{
       return id !== book.id
     })
     setBooks(updatedBooks)
   }
-  const editBookById=(id,newTitle)=>{
+  const editBookById= async (id,newTitle)=>{
+    const resp=await axios.put('http://localhost:3001/books/'+ id,{
+      title:newTitle
+    })
     const UpdatedBooks=Books.map((book)=>{
       if(book.id===id){
-        return {...book,title:newTitle}
+        return {...book,...resp.data}
       }
       return book
     })
